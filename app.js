d7c62c2ac5130c45d73b1acb3cf3d1dd9ebe3dfa
Ryan Backof <rmbacko92@gmail.com>
2021-03-10 20:48:51 -0500

    converted index to a module file and added module to app

---
+++ app.js
@@ -0,0 +1,4 @@
+var app=require('./index.js');
+
+processArgs=process.argv;
+app(processArgs);
--- index.js
+++ index.js
@@ -1,82 +1,84 @@
 //Car Command line
-var fs=require('fs');
-var commandLineArgLength=process.argv.length;
+module.exports= (processArgs) => {
+    var fs=require('fs');
+    var commandLineArgLength=processArgs.length;
 
-if(commandLineArgLength < 3)
-{
-    console.info("Syntax");
-    console.info("node carCommandLine <buy <dealershipName> <carnumber> |sell <dealershipName> <carnumber> |list (dealership|cars)| set <userfile> >");
-}
-else 
-{
-    firstCommand=process.argv[2];
-    if(firstCommand === 'buy')
+    if(commandLineArgLength < 3)
     {
-        if(commandLineArgLength < 5)
-        {
-            console.error('Specify a dealership and a car');
-        }
-        else
-        {
-            let dealer=process.argv[3];
-            let car=process.argv[4];
-            console.log("Buying a "+car+" from "+dealer);
-        }
+        console.info("Syntax");
+        console.info("node carCommandLine <buy <dealershipName> <carnumber> |sell <dealershipName> <carnumber> |list (dealership|cars)| set <userfile> >");
     }
-    else if (firstCommand === 'sell')
+    else 
     {
-        if(commandLineArgLength < 5)
+        firstCommand=processArgs[2];
+        if(firstCommand === 'buy')
         {
-            console.error('Specify a dealership and a car');
-        }
-        else
-        {
-            let dealer=process.argv[3];
-            let car=process.argv[4];
-            console.log("Selling your "+car+" to "+dealer);
-        }
-    }
-    else if(firstCommand === 'list')
-    {
-        if(commandLineArgLength < 4)
-        {
-            console.error('specify list "dealerships" or "cars"');
-        }
-        else
-        {
-            if(process.argv[3] === 'dealerships')
+            if(commandLineArgLength < 5)
             {
-            console.log("Listing all of the dealerships");
-            }
-            else if(process.argv[3] === 'cars')
-            {
-                if(commandLineArgLength < 5)
-                {
-                    console.error("Specify a dealership to list cars from");
-                }
-                else
-                {
-                    console.log("Listing all of the cars from "+process.argv[4]);
-                }
+                console.error('Specify a dealership and a car');
             }
             else
             {
-                console.log("Error choose cars or dealerships");
+                let dealer=processArgs[3];
+                let car=processArgs[4];
+                console.log("Buying a "+car+" from "+dealer);
             }
+        }
+        else if (firstCommand === 'sell')
+        {
+            if(commandLineArgLength < 5)
+            {
+                console.error('Specify a dealership and a car');
+            }
+            else
+            {
+                let dealer=processArgs[3];
+                let car=processArgs[4];
+                console.log("Selling your "+car+" to "+dealer);
+            }
+        }
+        else if(firstCommand === 'list')
+        {
+            if(commandLineArgLength < 4)
+            {
+                console.error('specify list "dealerships" or "cars"');
+            }
+            else
+            {
+                if(processArgs[3] === 'dealerships')
+                {
+                    console.log("Listing all of the dealerships");
+                }
+                else if(processArgs[3] === 'cars')
+                {
+                    if(commandLineArgLength < 5)
+                    {
+                        console.error("Specify a dealership to list cars from");
+                    }
+                    else
+                    {
+                        console.log("Listing all of the cars from "+processArgs[4]);
+                    }
+                }
+                else
+                {
+                    console.log("Error choose cars or dealerships");
+                }
         
+            }
         }
-    }
-    else if(firstCommand === 'set')
-    {
-        if(commandLineArgLength < 4)
+        else if(firstCommand === 'set')
         {
-            console.error('please enter in your user file');
+            if(commandLineArgLength < 4)
+            {
+                console.error('please enter in your user file');
+            }
+            else
+            {
+                let userFile=processArgs[3];
+                console.log("Importing data from "+userFile);
+            }
         }
-        else
-        {
-            let userFile=process.argv[3];
-            console.log("Importing data from "+userFile);
-        }
-    }
 
+    }
 }
